7e4a01be66107cc93c41c3390a2e6f0161b8972a
janelle mellor <janellemellor@gmail.com>
2020-04-28 16:11:30 -0700

    update UserInput to add button

--- src/components/UserInput/UserInput.jsx
+++ src/components/UserInput/UserInput.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const UserInput = ({ username, onUsernameChange }) => (
-  <>
+const UserInput = ({ username, onUsernameSubmit, onUsernameChange }) => (
+  <form onSubmit={onUsernameSubmit}>
     <input type='text' name='username' value={username} onChange={onUsernameChange} />
-  </>
+    <button>Find Github User!</button>
+  </form>
 );
 
 UserInput.propTypes = {
   username: PropTypes.string.isRequired,
+  onUsernameSubmit: PropTypes.func.isRequired,
   onUsernameChange: PropTypes.func.isRequired    
 };
 
