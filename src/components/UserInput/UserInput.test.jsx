4164f4c386e8ce202acc5920d0eab1a4c0f158db
janelle mellor <janellemellor@gmail.com>
2020-04-28 15:55:07 -0700

    add test for user input

---
+++ src/components/UserInput/UserInput.test.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import UserInput from './UserInput.jsx';
+
+describe('UserInput', () => {
+  it('matches a snapshot', () => {
+    const wrapper = shallow(<UserInput 
+      username='janellemellor'
+      onUsernameChange={() => {}}/>);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
