a7631595dc179a79983d18e0372d0cd0a373cfe3
janelle mellor <janellemellor@gmail.com>
2020-04-28 16:36:07 -0700

    setup container

---
+++ src/containers/GithubViewer/GithubViewer.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import UserInput from '../../components/UserInput/UserInput.jsx';
+import UserInfoDisplay from '../../components/UserInfoDisplay/UserInfoDisplay.jsx';
+// import UserRepoDisplay from '../../components/UserRepoDisplay/UserRepoDisplay.jsx';
+import { fetchGithubUser } from '../../services/gitHubApi.js';
+
+export default class GithubViewer extends Component {
+//   state = {}
+
+componentDidMount() {
+  
+}
+
+}
