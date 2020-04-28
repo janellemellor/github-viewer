88d984a8e0bc6d8c8e9ecac8460d0109815b6a59
janelle mellor <janellemellor@gmail.com>
2020-04-28 14:49:23 -0700

    updated pathing in index.js and imported UserInfoDisplay to App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
+import UserInfoDisplay from '../UserInfoDisplay/UserInfoDisplay.jsx';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return <UserInfoDisplay />;
 }
-  
