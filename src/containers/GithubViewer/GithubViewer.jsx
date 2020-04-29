195f944159a76a8ae58ba198fb017be13ad83721
janelle mellor <janellemellor@gmail.com>
2020-04-28 17:40:25 -0700

    add handle change function

--- src/containers/GithubViewer/GithubViewer.jsx
+++ src/containers/GithubViewer/GithubViewer.jsx
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
 import UserInput from '../../components/UserInput/UserInput.jsx';
 import UserInfoDisplay from '../../components/UserInfoDisplay/UserInfoDisplay.jsx';
-// import UserRepoDisplay from '../../components/UserRepoDisplay/UserRepoDisplay.jsx';
+import UserRepos from '../../components/UserRepos/UserRepos.jsx';
 import { fetchGithubUser } from '../../services/gitHubApi.js';
 
 export default class GithubViewer extends Component {
-//   state = {}
+    state = {
+      username: 'enter username'
+    }
+    
+    handleUsernameChange = ({ target }) => {
+      this.setState({ username: target.value });
+    };
 
-componentDidMount() {
-  
-}
+    handleUsernameSubmit
 
+    render() {
+      const { username } = this.state;
+      return (
+        <>
+          <UserInput username={username} onUsernameChange={this.handleUsernameChange} />
+        </>
+      );
+    }
 }
