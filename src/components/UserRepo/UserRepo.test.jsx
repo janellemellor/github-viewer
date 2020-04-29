122a3fbe6b84757e9a188517f6a232e30ea7f218
janelle mellor <janellemellor@gmail.com>
2020-04-28 17:02:13 -0700

    update test with new prop names

--- src/components/UserRepo/UserRepo.test.jsx
+++ src/components/UserRepo/UserRepo.test.jsx
@@ -4,7 +4,7 @@ import UserRepo from './UserRepo.jsx';
 
 describe('UserRepo', () => {
   it('matches a snapshot', () => {
-    const wrapper = shallow(<UserRepo repoName='backend' repoUrl='https://github.com/mental-health-twitterbot/backend' />);
+    const wrapper = shallow(<UserRepo name='backend' html_url='https://github.com/mental-health-twitterbot/backend' />);
     expect(wrapper).toMatchSnapshot();
   });
 });
