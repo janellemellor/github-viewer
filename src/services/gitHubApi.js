bfbf82f83c11371bfeacd120057754ff55022f59
janelle mellor <janellemellor@gmail.com>
2020-04-29 09:59:37 -0700

    update service function to only return selected results

--- src/services/gitHubApi.js
+++ src/services/gitHubApi.js
@@ -1,6 +1,12 @@
 export const fetchGithubUser = (username) => {
   return fetch(`https://api.github.com/users/${username}`)
-    .then(res => res.json());
-  // .then(json => json.results);
+    .then(res => res.json())
+    .then(json => ({
+      username: json.login,
+      followerCount: json.followers,
+      followingCount: json.following,
+      profileUrl: json.url
+    }));
 };
+
 
